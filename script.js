053cdf4b5813f2844eed8c37ff0db1ffb607ab67
HailGanyu <henryyanguk@gmail.com>
2022-07-11 13:28:34 +0100

    html grid made - to be moved into a js grid

---
+++ script.js
@@ -0,0 +1,8 @@
+
+document.addEventListener('gridMaker', function() {
+    var div = document.createElement('div');
+    div.id = 'grid';
+    div.innerHTML = "";
+ 
+    document.body.appendChild(div);
+}, false);
